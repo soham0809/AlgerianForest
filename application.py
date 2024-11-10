73fb94e84553ca25f675790cb612ff393f86beb8
Soham Joshi <soham0809>
2024-11-10 10:59:12 +0530

    Description of today's changes

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ from sklearn.preprocessing import StandardScaler
 
 application = Flask(__name__)
 app=application
-
+##TRYIN JUST TO MAKE CHANGES AT GIT
 ## import ridge regresor model and standard scaler pickle
 ridge_model=pickle.load(open('models/ridge.pkl','rb'))
 standard_scaler=pickle.load(open('models/scaler.pkl','rb'))
